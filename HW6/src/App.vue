b4060c0af94fe95fcc42bab4de223bdb05070ec6
Daniyar <baddaniar@gmail.com>
2023-02-09 21:31:10 +0600

    hw6 done

---
+++ HW6/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <nav>
+    <a href="/gallery">галерея</a>
+    <a href="/home">Главная</a>
+    <a href="/contacts">Контакты</a>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+  nav{
+    display: flex;
+    flex-direction: column;
+  }
+</style>
---
+++ HW6/src/pages/Contacts.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          name: "daniar",
+        },
+        {
+          id: 2,
+          name: "Aslan",
+        },
+        {
+          id: 3,
+          name: "Ilyas",
+        },
+        {
+          id: 4,
+          name: "Dymash",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <p>Контакты</p>
+  <ul>
+    <li v-for="user of users" :key="user.id">{{ user.name }}</li>
+  </ul>
+</template>
+
+<style></style>
